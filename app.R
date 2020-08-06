96f94ac6d7204b7222a2214316614e4b8a146cc9
Kaija Gahm <kaija.gahm@gmail.com>
2020-08-06 14:18:23 -0400

    moved stuff over to app.R from reactivitytest.R. This is now a working app.

--- app.R
+++ app.R
@@ -5,7 +5,8 @@ source("libraries.R")
 load(here("data", "s11.Rda"))
 s11 <- s11 %>%
   mutate(SentenceText = as.character(SentenceText),
-         Construction = as.character(Construction))
+         Construction = as.character(Construction)) %>%
+  mutate(label = paste(Gender, Age, Education, Income, Race, sep = "<br/>"))
 
 source("scripts/app_functions.R")
 
@@ -13,22 +14,22 @@ source("scripts/app_functions.R")
 ui <- shinyUI(fluidPage(
   
   #Add a title
-  titlePanel("YGDP Survey 11"),
   sidebarLayout(
-    
     # sidebar panel for inputs
     sidebarPanel(
-      
+      h1("YGDP Survey 11"),
       # Select a sentence
       p("Select a grammatical construction to explore:"),
       selectInput("construction", label = NULL, choices = unique(s11$Construction)),
       p("Select an example sentence:"),
-      selectInput("sentence", label = NULL, choices = unique(s11$SentenceText)), # hadley's example made it seem like the choices should start as NULL, so I don't know why this is working...
-      # If I change this to NULL, it breaks. I think it's because the filtering by ratings needs to work initially as well as once a selection has been made, but input$sentence is NULL until you've selected a construction.
+      selectInput("sentence", label = NULL, choices = unique(s11$SentenceText)),
+      actionButton("updatemap", "Update map"),
       
       # Select a demographic variable
+      br(),
       h4("Explore covariates (target sentence)"),
-      selectInput("covariate", "Choose a demographic variable:", choices = demographic_vars, selected = demographic_vars[1])
+      selectInput("covariate", "Choose a demographic variable:", choices = demographic_vars, selected = demographic_vars[1]),
+      actionButton("updateplot", "Update plot")
     ),
     
     # main panel for plots etc.
@@ -57,75 +58,84 @@ ui <- shinyUI(fluidPage(
 ))
 
 server <- shinyServer(function(input, output, session) {
-  # Data frame for the map: subset by sentence and rating limits
-  
-  constructiondf <- reactive({s11 %>%
-      filter(Construction == input$construction)
-  })
-  
-  observeEvent(constructiondf(), {
-    choices <- unique(constructiondf()$SentenceText)
-    updateSelectInput(session, "sentence", choices = choices)
-  })
-  
-  sentencedf <- reactive({
-    req(input$construction) # only proceed with this operation if there is a construction selected
-    constructiondf() %>% filter(SentenceText == input$sentence)
-  })
-  
-  sentencedf <- reactive({
-    s11 %>% filter(SentenceText == input$sentence)
-  })
-  
-  # Filter min/max ratings
-  mapdf <- reactive({sentencedf() %>% 
-      filter(SentenceText == input$sentence,
-             as.character(Judgment) %in% input$ratings) %>%
-      mutate(label = paste(Gender, Age, Education, Income, Race, sep = "<br/>"))
-  })
-  
   # Define a color palette
   pal <- colorFactor(
     palette = color_palette, # defined in app_functions.R
     domain = factor(s11$Judgment)
   )
   
+  # make a construction df
+  constructiondf <- reactive({s11 %>%
+      filter(Construction == input$construction)
+  })
+  
+  # when the construction df exists, generate new choices for the sentence menu
+  observeEvent(constructiondf(), {
+    choices <- unique(constructiondf()$SentenceText)
+    updateSelectInput(session, "sentence", choices = choices)
+  })
+  
+  # Define initial data for the map to load
+  initial_data <- isolate(s11 %>% filter(SentenceText == input$sentence))
+  
   output$mymap <- renderLeaflet({
-    leaflet(mapdf()) %>% 
+    leaflet() %>% 
       addProviderTiles(
         providers$CartoDB.Positron,
         options = providerTileOptions(minZoom = 4)) %>% # no zooming out
       setView(-96, 37.8, 4) %>%
-      addCircleMarkers(data = mapdf(), lat = ~Latitude, lng = ~ Longitude,
+      addCircleMarkers(data = initial_data, 
+                       lat = ~Latitude, lng = ~Longitude,
                        popup = ~label,
                        fillColor = ~rev(pal(Judgment)), 
                        color = "black",
                        weight = 0.5,
                        radius = 7, opacity = 1,
-                       fillOpacity = 0.5) #%>%
-    # addLegend_decreasing("bottomright", pal = pal, values = c("5", "4", "3", "2", "1"),
-    #           title = "Sentence rating",
-    #           opacity = 1,
-    #           decreasing = T) #%>%
-    #addControl(checkboxGroupInput("rSelect", label = "", choices = c("5", "4", "3", "2", "1"), selected = c("5", "4", "3", "2", "1"), width = '50%'), position = "topright") # commenting this out for now since I can't figure out how to merge it with the legend. Ultimately, this should replace the sliderInput "minmaxratings".
+                       fillOpacity = 0.5)
     
   })
   
-  # Sort by mean for only some variables
-  meansort <- reactive(ifelse(input$covariate %in% c("Gender", "Race", "RegANAE", "CarverReg"), T, F))
+  observeEvent(input$updatemap, { # when you click the "update map" button
+    # create an updated data frame
+    mapdf <- reactive({s11 %>% filter(SentenceText == input$sentence,
+                                      as.character(Judgment) %in% input$ratings)})
+    
+    # update the map with new points
+    leafletProxy("mymap") %>%
+      clearMarkers() %>%
+      addCircleMarkers(data = mapdf(), 
+                       lat = ~Latitude, lng = ~Longitude,
+                       popup = ~label,
+                       fillColor = ~rev(pal(Judgment)), 
+                       color = "black",
+                       weight = 0.5,
+                       radius = 7, opacity = 1,
+                       fillOpacity = 0.5)
+  })
   
   # Make a reactive expression for the selected demographic covariate name
-  covariateTitleName <- reactive(names(demographic_vars[which(demographic_vars == input$covariate)]))
   
-  # Barplot of the demographic variable chosen
-  output$barplot <- renderPlot({
-    sentence_barplot(df = sentencedf(), xvar = input$covariate, yvar = "Judgment", show_legend = F, xlab = T, order_by_mean = meansort()) +
-      theme(text = element_text(size = 17))+
-      scale_color_manual(color_palette)+
-      ggtitle(paste0("Sentence judgments by ", # convert to lower if not Carver
-                     if(covariateTitleName() %in% c("Carver dialect region", "ANAE dialect region")){covariateTitleName()}else{tolower(as.character(covariateTitleName()))})
-      )
+  
+  observeEvent(input$updatemap, { # when you click the "update map" button
+    # create an updated data frame
+    covariateTitleName <- reactive(names(demographic_vars[which(demographic_vars == input$covariate)]))
+    plotdf <- reactive({s11 %>% filter(SentenceText == input$sentence)})
+    
+    # Barplot of the demographic variable chosen
+    output$barplot <- renderPlot({
+      sentence_barplot(df = s11 %>% filter(SentenceText == input$sentence), 
+                       xvar = input$covariate, yvar = "Judgment", show_legend = F, xlab = T, 
+                       #order_by_mean = meansort()
+      ) +
+        theme(text = element_text(size = 17))+
+        scale_color_manual(color_palette)+
+        ggtitle(paste0("Sentence judgments by ", # convert to lower if not Carver
+                       if(covariateTitleName() %in% c("Carver dialect region", "ANAE dialect region")){covariateTitleName()}else{tolower(as.character(covariateTitleName()))})
+        )
+    })
+    
   })
+  
 })
 
 #generic line that launches the app
