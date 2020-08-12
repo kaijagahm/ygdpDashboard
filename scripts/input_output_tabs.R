7513b336dcf6433f2412847a7351d1501175dd75
Kaija Gahm <kaija.gahm@gmail.com>
2020-08-12 17:01:43 -0400

    added parameters for boolean map view

--- scripts/input_output_tabs.R
+++ scripts/input_output_tabs.R
@@ -1,25 +1,42 @@
 # input tabset
+initialConstruction <- "Come with"
+initialSentence <- "If I go to the store, do you want to come with?"
+initialSentence2 <- "If you go there, bring a friend with."
+initialCovariate <- demographic_vars[1]
+  
 input_control_tabs <- tabsetPanel(
   id = "inputControls",
   type = "hidden",
   tabPanel("map",
            div(htmlDependency("font-awesome", "5.13.0", "www/shared/fontawesome", package = "shiny", 
                               stylesheet = c("css/all.min.css", "css/v4-shims.min.css")), # help idk what this does
-               checkboxGroupInput(inputId = "ratings",
-                                  label = "Filter by rating", # name of the checkbox panel
+               checkboxGroupInput(inputId = "ratings", label = "Filter by rating", 
                                   choiceNames = htmlchoicenames, # from app_functions.R
                                   choiceValues = 5:1, selected = 5:1)
            ),
-           actionButton("updatemap", "Update map")
+           actionButton("updateMap", "Update map")
   ),
   tabPanel("plot", 
            # Select covariate
            selectInput("covariate", "Choose a demographic variable:", 
                        choices = demographic_vars, selected = demographic_vars[1]),
-           actionButton("updateplot", "Update plot")
+           actionButton("updatePlot", "Update plot")
   ),
   tabPanel("mapBoolean",
-           
+           h3("Map parameters"),
+           checkboxGroupInput("allowratings1", "Allow ratings (sentence 1)",
+                              choiceNames = c("5", "4", "3", "2", "1"),
+                              choiceValues = 5:1, selected = 5:1,
+                              inline = T),
+           selectInput("construction2", "Choose a second construction:", choices = unique(s11$Construction), 
+                       selected = "Come with"),
+           # Select a sentence
+           htmlOutput("sentence2Selector"), # this will be generated from the server
+           checkboxGroupInput("allowratings2", "Allow ratings (sentence 2)",
+                              choiceNames = c("5", "4", "3", "2", "1"),
+                              choiceValues = 5:1, selected = 5:1,
+                              inline = T),
+           actionButton("updateMapBoolean", "Update map")
   )
 )
 
@@ -34,7 +51,7 @@ output_view_tabs <- tabsetPanel(
   tabPanel("plot",
            plotOutput("plot")
            ),
-  tabPanel("mapBoolean"#,
-           #output$mapBoolean
+  tabPanel("mapBoolean",
+           leafletOutput("mapBoolean")
            )
 )
