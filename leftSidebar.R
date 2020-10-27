0ac4b5120beaca17dd5510a1c7780910ec19c66d
Kaija Gahm <kaija.gahm@gmail.com>
2020-10-27 15:31:59 -0400

    Added interpolation mode and adjusted width. Updated some headers. Updated sentence choices for sentence 1.

--- leftSidebar.R
+++ leftSidebar.R
@@ -1,38 +1,53 @@
 # Dashboard left sidebar
 source("dashboardFunctions.R")
 
+leftSidebarWidth <- 250
+
 LEFTSIDEBAR <- dashboardSidebar(
   leftSidebarScroll, # CSS to make the sidebar scroll. See dashboardFunctions.R
+  width = leftSidebarWidth,
   sidebarMenu(
     id = "leftSidebar", # name of the left sidebar
-    menuItem(expandedName = "mapView", text = "Map view",  icon = icon("map"), startExpanded = TRUE,
+    
+    # POINTS MODE -----------------------------------------------------------
+    menuItem(expandedName = "pointMaps", text = "Point maps",  icon = icon("map"), 
+             startExpanded = TRUE,
              # Title
              h4("Select sentences"),
              # Inputs
              div(style = reduceSpacing,
                  selectInput("survey", "Survey", 
-                             choices = c("5", "5b", "6", "6b", "7", "8", "9", "11", "12"),
-                             selected = "11",
+                             choices = str_replace(names(sentencesNestedList), "^S", ""),
+                             selected = str_replace(names(sentencesNestedList), "^S", "")[1],
                              multiple = FALSE)),
              ### Sentence selector
-             div(style = reduceSpacing,
+             div(id = "sentence1controls", div(style = reduceSpacing,
                  selectizeInput("sentence1", "Sentence 1:",
-                                choices = list(
-                                  `After perfect` = c("I'm after eating ice cream.",
-                                                      "She's just after telling me that she got the promotion."),
-                                  `Needs washed` = c("Most babies like cuddled.",
-                                                     "My car needs washed.")
-                                ), multiple = F)),
+                                choices = str_extract(names(sentencesNestedList[[1]]), 
+                                                      "(?<=\\_).*$"),
+                                selected = str_extract(names(sentencesNestedList[[1]]), 
+                                                       "(?<=\\_).*$")[1], 
+                                multiple = F)),
              prettyRatingSelector(sentenceNum = 1), # defined in dashboardFunctions.R
              hr(),
-             uiOutput("sentence2Controls"), # controls for the second sentence
-             uiOutput("sentence3Controls"), # controls for the third sentence
-             br(),
+             br()
+             ), # end of div "sentence1controls"
              actionButton("addSentence", "+ Add a sentence"), # button to add the second sentence
              div(style="display:inline-block", actionButton("sentencesReset", "Reset all", style = "background-color: #4AA8F7")),
              div(style="display:inline-block", actionButton("sentencesApply", "Apply", style = "background-color: #A8F74A"))
     ),
-    menuItem(text = "Social variables", expandedName = "socialVariables", icon = icon("map"), startExpanded = FALSE,
+
+    # INTERPOLATION MODE ------------------------------------------------------
+    menuItem(text = "Interpolation maps", expandedName = "interpolationMaps", 
+             icon = icon("map"),
+             startExpanded = FALSE,
+             # Title
+             h4("Select sentences")
+             ),
+    
+    # SOCIAL VARIABLES MODE ---------------------------------------------------
+    menuItem(text = "Social variables", expandedName = "socialVariables", 
+             icon = icon("map"), startExpanded = FALSE,
              div(style = reduceSpacing,
                  selectInput("test", "Test", 
                              choices = c("a", "b", "c", "d", "e"),
