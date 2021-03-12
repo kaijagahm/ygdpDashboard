b896573a3f08c8585dc3ab055466b9faa0d31f20
kaijagahm <kaija.gahm@gmail.com>
2021-03-12 16:44:20 -0500

    added a new test title

--- shinydashboard.R
+++ shinydashboard.R
@@ -82,7 +82,7 @@ ui <- function(request){ # Defined this as a function so that URL bookmarking wo
     
     # HEADER ------------------------------------------------------------------
     header = dashboardHeaderPlus(
-      title = "YGDP Data Explorer",
+      title = "TEST TITLE",
       #XXX commented out bookmark button because bookmarking isn't working. See github issue 33 for more details.
       # tags$li(class = "dropdown", 
       #         bookmarkButton(label = "Bookmark app state")),
